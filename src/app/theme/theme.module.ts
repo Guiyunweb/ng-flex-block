8faeba9761efbc3e0c7814865ed54d506532c479
Guiyun <i@guiyunweb.com>
2020-12-07 13:52:28 +0800

    fix:修改主页

--- src/app/theme/theme.module.ts
+++ src/app/theme/theme.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
 import { NavComponent } from './layout/nav/nav.component';
 import { HeaderComponent } from './layout/header/header.component';
+import {AppRoutingModule} from '../routes/app-routing.module';
 
 
 
 @NgModule({
   declarations: [LayoutComponent, NavComponent, HeaderComponent],
-  imports: [
-    CommonModule
-  ]
+    imports: [
+        CommonModule,
+        AppRoutingModule
+    ]
 })
 export class ThemeModule { }
