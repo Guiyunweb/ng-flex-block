e60f725abcc5900e98d690bcc4f9a28d593513f6
Guiyun <i@guiyunweb.com>
2020-12-07 11:48:05 +0800

    feat:添加头部文件

--- src/app/theme/theme.module.ts
+++ src/app/theme/theme.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
+import { NavComponent } from './layout/nav/nav.component';
+import { HeaderComponent } from './layout/header/header.component';
 
 
 
 @NgModule({
-  declarations: [LayoutComponent],
+  declarations: [LayoutComponent, NavComponent, HeaderComponent],
   imports: [
     CommonModule
   ]
