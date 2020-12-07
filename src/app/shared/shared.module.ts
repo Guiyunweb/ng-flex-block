dd9961840dcab97a8a16093e3401dee4e6fd3c73
Guiyun <i@guiyunweb.com>
2020-12-07 14:10:05 +0800

    feat:添加文章卡片

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ArticleCardComponent } from './article-card/article-card.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ArticleCardComponent],
+  exports: [
+    ArticleCardComponent
+  ],
   imports: [
     CommonModule
   ]
